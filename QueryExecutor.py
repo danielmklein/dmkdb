bb53e6f370f852d39e60f9377c1cec8f07f7aa5f
dmarklein <daniel.klein.dmk@gmail.com>
2015-04-26 12:38:16 -0600

    it's all working. just need to tidy up and test

--- QueryExecutor.py
+++ QueryExecutor.py
@@ -20,7 +20,7 @@ class QueryExecutor(object):
         columns = [col.strip() for col in columns.split(",")]
         print("columns to fetch are: {0}".format(columns))
         
-        table = re.search(r"FROM\s+([A-Z])\s+", query).group(1).strip()
+        table = re.search(r"FROM\s+([A-Z])\s*", query).group(1).strip()
         print("table to query is: {0}".format(table))
 
         conditions_match = re.search(r"WHERE\s+([ABCDNOR()=<>0-9 ]+)(ORDER BY.*)?;", query)
@@ -39,6 +39,8 @@ class QueryExecutor(object):
         print()
 
         return (columns, table, conditions, order_by)
+    # end parse_query
+    
 
 
     def perform_query(self, query):
@@ -52,63 +54,122 @@ class QueryExecutor(object):
         conditions = query_params[2]
         order_by = query_params[3]
 
-        result = []
-
         if (table_name not in self.db.tables.keys()):
             raise Exception("invalid table name in query: {0}".format(query))
 
         # set the name of the table to use
         data_table = self.db.tables[table_name]
 
+        # this is a dict
+        selected_tuples = self.select(conditions, data_table) 
+
+        # this is a list of lists
+        sorted_tuples = self.sort(data_table.column_names, selected_tuples, order_by) 
+
         # set the columns we are gonna use
         if (len(col_names) == 1 and col_names[0] == "*"):
             col_names = data_table.column_names
 
+        # project the columns we want
+        print("we want columns: {0}".format(col_names))
+        
+        if (len(col_names) == 1 and re.search(r"[A-Z]+\([A-Z0-9]+\)", col_names[0])):
+            agg_match = re.search(r"([A-Z]+)\(([A-Z0-9]+)\)", col_names[0])
+            agg_func = agg_match.group(1).strip()
+            agg_col = agg_match.group(2).strip()
+            print("we want to find the {} of column {}".format(agg_func, agg_col))
+            result = self.perform_aggregate(agg_func, agg_col, data_table.column_names, sorted_tuples)
+        else:
+
+            for col in col_names: # TODO: MAKE SURE WE DO AVG!!
+                if (col not in data_table.column_names):
+                    raise Exception("invalid column name: {0}".format(col))
+
+            result = self.project(col_names, data_table.column_names, sorted_tuples)
+ 
+        print("results: {0}".format(result))
+
+        return col_names, result
+    # end perform_query
+
+    
+    '''
+    select the tuples that match the WHERE conditions
+    '''
+    def select(self, conditions, data_table):
+        selected_tuples = {}
         if (conditions):
             # evaluate the conditional expression -- filter out tuples
             evaluator = ConditionEvaluator()
-            selected_tuples = {}
-       
+            
             for key in data_table.rows.keys():
                 cur_row = data_table.rows[key]
                 if (evaluator.evaluate_condition(cur_row, conditions)):
                     print("column {0} matches condition, added to result".format(cur_row))
                     selected_tuples[key] = cur_row
+        else:
+            selected_tuples = data_table.rows
 
-        # sort by the column we wish to order by
-        sorted_tuples = []
-        for key in selected_tuples.keys():
-            row = []
-            for col in data_table.column_names:
-                row.append(int(selected_tuples[key][col]))
-            sorted_tuples.append(row)
-        
-        if (order_by):
-            sort_idx = data_table.column_names.index(order_by)
-            sorted_tuples.sort(key = lambda tup: tup[sort_idx])
+        return selected_tuples
+    # end select
 
-        # project the columns we want
-        print("we want columns: {0}".format(col_names))
-        for col in col_names: # TODO: MAKE SURE WE DO AVG!!
-            if (col not in data_table.column_names):
-                raise Exception("invalid column name: {0}".format(col))
-
+    
+    '''
+    project the columns specified in the SELECT clause
+    '''
+    def project(self, proj_cols, all_col_names, tuples):
         projected = []
         col_indexes = []
-        for col in col_names:
-            col_indexes.append(data_table.column_names.index(col))
+        for col in proj_cols:
+            col_indexes.append(all_col_names.index(col))
 
-        for row in sorted_tuples:
+        for row in tuples:
             projected_row = []
             for col_idx in col_indexes:
                 projected_row.append(row[col_idx])
             projected.append(projected_row)
 
-        print("results: {0}".format(projected))
-
-        return col_names, projected
+        return projected
+    # end project
             
+    
+    def sort(self, col_names, unordered_tuples, order_by):
+        # sort by the column we wish to order by
+        sorted_tuples = []
+        for key in unordered_tuples.keys():
+            row = []
+            for col in col_names:
+                row.append(int(unordered_tuples[key][col]))
+            sorted_tuples.append(row)
         
+        if (order_by):
+            sort_idx = col_names.index(order_by)
+            sorted_tuples.sort(key = lambda tup: tup[sort_idx])
+        
+        return sorted_tuples
+    # end sort
+
+  
+    def perform_aggregate(self, func_name, col, col_names, tuples,):
+        
+        if func_name != "AVG":
+            raise Exception("aggregate function: {} is not supported.".format(func_name))
+
+        col_index = col_names.index(col)
+        sum = 0
+
+        for row in tuples:
+            sum += row[col_index]
+
+        if len(tuples) == 0:
+            result = []
+        else:
+            result = [["{0:.2f}".format(sum/len(tuples))]]
+            for num in result:
+                print("avg: {}".format(num))
+
+        return result
+    # end perform_aggregate
 
 
 
